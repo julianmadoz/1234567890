7b5100f6ba0350b4de11b55731852bea43e54b8e
Julian Madoz <julianmadoz@Julians-MacBook.local>
2020-08-22 23:22:21 -0300

    chau yt

--- YouTube.js
+++
@@ -1,6 +0,0 @@
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyCF6BDKIVuMi-64nWVTz8Jf-_g-xN5HOaM");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
